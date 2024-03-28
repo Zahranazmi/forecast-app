e1be2d5936214f1898b4ca85eac60755a0c0fa45
Zahranazmi <135511535+Zahranazmi@users.noreply.github.com>
2024-03-28 03:10:07 +0100

    Injected more weather data

--- src/index.js
+++ src/index.js
@@ -2,9 +2,61 @@ function refreshWeather(response) {
   let temperatureElement = document.querySelector("#temperature");
   let temperature = response.data.temperature.current;
   let cityElement = document.querySelector("#city");
+  let conditionElement = document.querySelector("#condition");
+  let humidityElement = document.querySelector("#humidity");
+  let feelsLikeElement = document.querySelector("#feels-like");
+  let timeElement = document.querySelector("#time");
+  let date = new Date(response.data.time * 1000);
 
+  timeElement.innerHTML = formatDate(date);
   cityElement.innerHTML = response.data.city.toUpperCase().trim();
   temperatureElement.innerHTML = Math.round(temperature);
+  conditionElement.innerHTML =
+    response.data.condition.description.toUpperCase();
+  humidityElement.innerHTML = response.data.temperature.humidity;
+  feelsLikeElement.innerHTML = Math.round(response.data.temperature.feels_like);
+}
+
+function formatDate(date) {
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+  let dateDay = date.getDate();
+
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+
+  let months = [
+    "January",
+    "Febuary",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  let month = months[date.getMonth()];
+
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+
+  return `${day}, ${dateDay} ${month} &nbsp ${hours}:${minutes}`;
 }
 
 function searchCity(city) {
@@ -22,4 +74,4 @@ function handleSearchSubmit(event) {
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
-searchCity("tabriz");
+searchCity("Tabriz");
